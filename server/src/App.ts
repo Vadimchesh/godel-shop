8c39d644729158fd651f20ac2714b5fe431e1a0b
Vadimchesh <vadimlindo@gmail.com>
2021-03-14 22:38:01 +0300

    P1-3: delet cors

--- server/src/App.ts
+++ server/src/App.ts
@@ -1,13 +1,11 @@
 import express, { Express } from "express"
 import mongoose from "mongoose"
-import cors from "cors"
 import postRoutes from "./routes"
 
 const app: Express = express()
 
 const PORT: string | number = process.env.PORT || 4000
 
-app.use(cors())
 app.use(express.json())
 app.use(postRoutes)
 
